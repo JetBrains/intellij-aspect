cb50295f648aa25850a1ad12b3b26bee710540d4
Klaus T. Aehlig <klaus.aehlig@jetbrains.com>
2026-04-24 12:43:22 +0200

    differ: allow the user to specify the fields to be ignored
    This aspect should serve as a unifying aspect for many different
    aspects, hence the set of fields that are "new" depends on the
    aspect taken as reference.

--- tools/differ/main.kt
+++ tools/differ/main.kt
@@ -93,6 +93,13 @@ fun main(args: Array<String>) {
     description = "Languages to deploy the current aspect for",
   )
 
+  val newFields by parser.option(
+    ArgType.String,
+    shortName = "n",
+    fullName = "new-fields",
+    description = "Path prefixes (i.e., message subtrees) to consider new in the current aspect",
+  ).default("workspace_name,build_file_artifact_location")
+
   parser.parse(args)
 
   try {
@@ -133,7 +140,8 @@ fun main(args: Array<String>) {
       val rawResult = compareTargets(referenceTargets, currentTargets)
 
       // Apply exception filters to suppress known benign differences
-      val filterResult = filterDifferences(rawResult.differences, DefaultFilters.ALL)
+      val filters = DefaultFilters.ALL + listOf(DefaultFilters.newFields(newFields))
+      val filterResult = filterDifferences(rawResult.differences, filters)
       System.err.println("Filtered ${filterResult.filtered.values.sumOf { it.size }} benign differences")
 
       // Create filtered comparison result
