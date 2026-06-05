c189126886fed433db7926526f97c36d44680626
Daniel Brauner <daniel.brauner@jetbrains.com>
2026-06-05 17:00:52 +0200

    fixed formatting

--- sdk/lib/aspectchoice.kt
+++ sdk/lib/aspectchoice.kt
@@ -23,24 +23,58 @@ enum class Rules(val rulesetName: String) {
   KOTLIN("@rules_kotlin"),
   SCALA("@rules_scala"),
   GO("@rules_go"),
-  PROTO("@protobuf");
+  PROTO("@protobuf"),
 }
 
 /**
  * Aspects in correct (topological) order together with the languages for which they should be present.
  */
 enum class Aspects(val pkg: String, val file: String, val aspect: String, private val rules: Set<Rules>) {
-  PROTOBUF("modules", "protobuf_info.bzl", "intellij_protobuf_info_aspect", setOf(Rules.PROTO)),
-  CC("modules", "cc_info.bzl", "intellij_cc_info_aspect", setOf(Rules.CC)),
-  PY("modules", "py_info.bzl", "intellij_py_info_aspect", setOf(Rules.PYTHON)),
-  PYTHON("modules", "python_info.bzl", "intellij_python_info_aspect", setOf(Rules.PYTHON)),
-  JAVA("modules", "java_info.bzl", "intellij_java_info_aspect", setOf(Rules.JAVA)),
-  KOTLIN("modules", "kotlin_info.bzl", "intellij_kotlin_info_aspect", setOf(Rules.KOTLIN)),
-  SCALA("modules", "scala_info.bzl", "intellij_scala_info_aspect", setOf(Rules.SCALA)),
-  JVM("modules", "jvm_info.bzl", "intellij_jvm_info_aspect", setOf(Rules.JAVA, Rules.KOTLIN, Rules.SCALA)),
-  JAVA_COMMON("modules", "java_common_info.bzl", "intellij_java_common_info_aspect", setOf(Rules.JAVA, Rules.KOTLIN, Rules.SCALA)),
-  GO("modules", "go_info.bzl", "intellij_go_info_aspect", setOf(Rules.GO)),
-  INTELLIJ("intellij", "aspect.bzl", "intellij_info_aspect", Rules.entries.toSet());
+  PROTOBUF(
+    "modules", "protobuf_info.bzl", "intellij_protobuf_info_aspect",
+    setOf(Rules.PROTO),
+  ),
+  CC(
+    "modules", "cc_info.bzl", "intellij_cc_info_aspect",
+    setOf(Rules.CC),
+  ),
+  PY(
+    "modules", "py_info.bzl", "intellij_py_info_aspect",
+    setOf(Rules.PYTHON),
+  ),
+  PYTHON(
+    "modules", "python_info.bzl", "intellij_python_info_aspect",
+    setOf(Rules.PYTHON),
+  ),
+  JAVA(
+    "modules", "java_info.bzl", "intellij_java_info_aspect",
+    setOf(Rules.JAVA),
+  ),
+  KOTLIN(
+    "modules", "kotlin_info.bzl", "intellij_kotlin_info_aspect",
+    setOf(Rules.KOTLIN),
+  ),
+  SCALA(
+    "modules", "scala_info.bzl", "intellij_scala_info_aspect",
+    setOf(Rules.SCALA),
+  ),
+  JVM(
+    "modules", "jvm_info.bzl", "intellij_jvm_info_aspect",
+    setOf(Rules.JAVA, Rules.KOTLIN, Rules.SCALA),
+  ),
+  JAVA_COMMON(
+    "modules", "java_common_info.bzl", "intellij_java_common_info_aspect",
+    setOf(Rules.JAVA, Rules.KOTLIN, Rules.SCALA),
+  ),
+  GO(
+    "modules", "go_info.bzl", "intellij_go_info_aspect",
+    setOf(Rules.GO),
+  ),
+  INTELLIJ(
+    "intellij", "aspect.bzl", "intellij_info_aspect",
+    Rules.entries.toSet(),
+  ),
+  ;
 
   override fun toString(): String {
     return "$pkg:$file%$aspect"
