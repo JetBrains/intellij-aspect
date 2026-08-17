6a4e9d8e870601bfd8c33da52ddae1a322012dd8
Klaus T. Aehlig <klaus.aehlig@jetbrains.com>
2026-08-17 12:37:57 +0200

    Add basic test that transitive dependencies are not reported as direct deps

--- testing/fixtures/java/simple/lib/Util.java
+++ testing/fixtures/java/simple/lib/Util.java
@@ -1,4 +1,9 @@
 package com.intellij.aspect.testing.fixtures.java.simple.lib;
 
+import com.intellij.aspect.testing.fixtures.java.simple.lib.MoreUtils;
+
 public class Util {
+   void use() {
+       MoreUtils utils = new MoreUtils();
+   }
 }
